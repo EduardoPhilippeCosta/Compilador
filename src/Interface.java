3595d8ef955194417014175c2c44d7e521f4eb89
VitorEduard <39387199+VitorEduard@users.noreply.github.com>
2021-08-24 21:57:12 -0300

    Criado layout e renomeado
    Colocado panel e layout pro toolbar.
    Renomeado os objetos da interface.

--- src/Interface.java
+++ src/Interface.java
@@ -3,16 +3,25 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JToolBar;
-import javax.swing.ScrollPaneConstants;
 
 import java.awt.BorderLayout;
-import java.awt.Window.Type;
 import javax.swing.SwingConstants;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JSplitPane;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class Interface {
 
@@ -50,52 +59,69 @@ public class Interface {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension frameMin = new Dimension(900,600);
 	    frame.setMinimumSize(frameMin);
+	    frame.getContentPane().setLayout(null);
 	    
 	    JToolBar toolBar = new JToolBar();
+	    toolBar.setBounds(0, 0, 143, 547);
 	    toolBar.setOrientation(SwingConstants.VERTICAL);
-	    frame.getContentPane().add(toolBar, BorderLayout.WEST);
+	    frame.getContentPane().add(toolBar);
 	    Dimension toolBarMin = new Dimension(150,500);
 	    toolBar.setMinimumSize(toolBarMin);
 	    
-	    JButton btnNewButton = new JButton("Novo [CTRL-N]");
-	    toolBar.add(btnNewButton);
-	    JButton btnNewButton2 = new JButton("Abrir [CTRL-O]");
-	    toolBar.add(btnNewButton2);
-	    JButton btnNewButton3 = new JButton("Salvar [CTRL-S]");
-	    toolBar.add(btnNewButton3);
-	    JButton btnNewButton4 = new JButton("Copiar [CTRL-C]");
-	    toolBar.add(btnNewButton4);
-	    JButton btnNewButton5 = new JButton("Colar [CTRL-V]");
-	    toolBar.add(btnNewButton5);
-	    JButton btnNewButton6 = new JButton("Recortar [CTRL-X]");
-	    toolBar.add(btnNewButton6);
-	    JButton btnNewButton7 = new JButton("Compilar [F7]");
-	    toolBar.add(btnNewButton7);
-	    JButton btnNewButton8 = new JButton("Equipe [F1]");
-	    toolBar.add(btnNewButton8);
+	    JPanel panelToolBar = new JPanel();
+	    toolBar.add(panelToolBar);
 	    
-	    JLabel lblNewLabel = new JLabel("New label");
-	    frame.getContentPane().add(lblNewLabel, BorderLayout.SOUTH);
+	    JButton btnNovo = new JButton("Novo [CTRL-N]");
+	    btnNovo.setBounds(18, 5, 108, 23);
+	    btnNovo.setIcon(new ImageIcon("D:\\WorkSpace\\Eclipse\\Compilador\\img\\btn_novo.ico"));
+	    JButton btnAbrir = new JButton("Abrir [CTRL-O]");
+	    btnAbrir.setBounds(19, 33, 103, 23);
+	    JButton btnSalvar = new JButton("Salvar [CTRL-S]");
+	    btnSalvar.setBounds(16, 61, 109, 23);
+	    JButton btnCopiar = new JButton("Copiar [CTRL-C]");
+	    btnCopiar.setBounds(15, 89, 111, 23);
+	    JButton btnColar = new JButton("Colar [CTRL-V]");
+	    btnColar.setBounds(19, 117, 103, 23);
+	    JButton btnRecortar = new JButton("Recortar [CTRL-X]");
+	    btnRecortar.setBounds(10, 145, 121, 23);
+	    JButton btnCompilar = new JButton("Compilar [F7]");
+	    btnCompilar.setBounds(22, 173, 97, 23);
+	    JButton btnEquipe = new JButton("Equipe [F1]");
+	    btnEquipe.setBounds(18, 202, 104, 23);
+	    btnEquipe.setIcon(new ImageIcon("D:\\WorkSpace\\Eclipse\\Compilador\\img\\btn_novo.ico"));
+	    panelToolBar.setLayout(null);
+	    panelToolBar.add(btnNovo);
+	    panelToolBar.add(btnAbrir);
+	    panelToolBar.add(btnSalvar);
+	    panelToolBar.add(btnCopiar);
+	    panelToolBar.add(btnColar);
+	    panelToolBar.add(btnRecortar);
+	    panelToolBar.add(btnCompilar);
+	    panelToolBar.add(btnEquipe);
+	    
+	    JLabel lblArquivo = new JLabel("Arquivo aberto");
+	    lblArquivo.setBounds(0, 547, 884, 14);
+	    frame.getContentPane().add(lblArquivo);
 	    Dimension labelMin = new Dimension(900,25);
-	    lblNewLabel.setMinimumSize(labelMin);
+	    lblArquivo.setMinimumSize(labelMin);
 	    
 	    JSplitPane splitPane = new JSplitPane();
+	    splitPane.setBounds(143, 0, 741, 547);
 	    splitPane.setResizeWeight(0.8);
 	    splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
-	    frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+	    frame.getContentPane().add(splitPane);
 	    
-	    JTextArea textArea_1 = new JTextArea();
-	    splitPane.setLeftComponent(textArea_1);  
-	    textArea_1.setBorder(new NumberedBorder());
-	    JScrollPane scroll1 = new JScrollPane (textArea_1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-	    splitPane.add(scroll1);
+	    JTextArea textAreaCaixaDeTexto = new JTextArea();
+	    splitPane.setLeftComponent(textAreaCaixaDeTexto);  
+	    textAreaCaixaDeTexto.setBorder(new NumberedBorder());
+	    JScrollPane scrollCaixaDeTexto = new JScrollPane (textAreaCaixaDeTexto, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+	    splitPane.add(scrollCaixaDeTexto);
 	    
-	    JTextArea textArea_2 = new JTextArea();
-	    textArea_2.setEditable(false);
-	    splitPane.setRightComponent(textArea_2);
-	    JScrollPane scroll2 = new JScrollPane (textArea_2, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-	    splitPane.add(scroll2);
+	    JTextArea textAreaConsole = new JTextArea();
+	    textAreaConsole.setEditable(false);
+	    splitPane.setRightComponent(textAreaConsole);
+	    JScrollPane scrollConsole = new JScrollPane (textAreaConsole, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+	    splitPane.add(scrollConsole);
 	    
 	}
-
 }
