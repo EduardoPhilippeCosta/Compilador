bc579f9d16406fe24c7ad73f2ca95cee27f5a524
VitorEduard <39387199+VitorEduard@users.noreply.github.com>
2021-11-23 21:46:57 -0300

    Acao 1 e 17

--- src/controller/Semantico.java
+++ src/controller/Semantico.java
@@ -20,23 +20,29 @@ public class Semantico implements Constants
     	codigo.append("\n");
         try {
 			switch (action) {
-			case 5:
-				acao5(token);
-				break;
-			case 6:
-				acao6(token);
-				break;
-			case 14:
-				acao14();
-				break;
-			case 15:
-				acao15();
-				break;
-			case 16:
-				acao16();
-				break;
-			default:
-				throw new SemanticError("AÃ§Ã£o semÃ¢ntica nÃ£o implementada: " + action);
+				case 1:
+					acao1();
+					break;
+				case 5:
+					acao5(token);
+					break;
+				case 6:
+					acao6(token);
+					break;
+				case 14:
+					acao14();
+					break;
+				case 15:
+					acao15();
+					break;
+				case 16:
+					acao16();
+					break;
+				case 17:
+					acao17(token);
+					break;
+				default:
+					throw new SemanticError("Ação semântica não implementada: " + action);
 			}
 		} catch (Exception e) {
 			throw new SemanticError(e.getMessage(), token.getPosition(), token.getLine());
@@ -58,9 +64,8 @@ public class Semantico implements Constants
     	Tipo tipo = pilha_tipos.pop();
     	if (tipo.equals(Tipo.INT)) {
     		codigo.append("conv.i8");
-    	} else {
-    		codigo.append("call void [mscorlib]System.Console::Write(").append(tipo.getTipo()).append(")");
     	}
+    	codigo.append("call void [mscorlib]System.Console::Write(").append(tipo.getTipo()).append(")");
     }
     
     public void acao15() {
@@ -74,8 +79,58 @@ public class Semantico implements Constants
     }
     
     public void acao16() {
-    	codigo.append("\n\t\tret");
-    	codigo.append("\n}");
+    	codigo.append("\t\tret");
+    	codigo.append("\n\t}");
     	codigo.append("\n}");
     }
+    
+
+    public void acao1() throws Exception {
+    	Tipo tipo1 = pilha_tipos.pop();
+    	Tipo tipo2 = pilha_tipos.pop();
+    	boolean tipo1Correto = tipo1.equals(Tipo.FLOAT) || tipo1.equals(Tipo.INT);
+    	boolean tipo2Correto = tipo2.equals(Tipo.FLOAT) || tipo2.equals(Tipo.INT);
+    	if (!tipo1Correto && !tipo2Correto) {
+    		throw new Exception("tipos incompatíveis em expressão aritmética.");
+    	} else if(!tipo1Correto || !tipo2Correto) {
+    		throw new Exception("tipo incompatível em expressão aritmética.");
+    	}
+    	
+    	if (tipo1 == Tipo.FLOAT || tipo2 == Tipo.FLOAT) {
+    		pilha_tipos.push(Tipo.FLOAT);
+    	} else {
+    		pilha_tipos.push(Tipo.INT);
+    	}
+    	
+    	codigo.append("add");
+    }
+
+    public void acao17(Token token) {
+    	pilha_tipos.push(Tipo.STRING);
+    	codigo.append("ldstr ").append(token.getLexeme());
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
